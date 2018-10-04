46ec514674ef023480200f053c8d14d8b934a1c4
Meredith <mhu2@stuy.edu>
2018-10-04 14:51:50 -0400

    started it, will add actual tests later

---
+++ heap_funcs/heap_funcs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  /*make a pointer to an array */
+  int *array;
+  
+  printf("Testing calloc:\n");
+  /*testing calloc, a function that dynamically allocates memory and sets it to 0*/
+  
+  
+  printf("\n\n----------------\n\n");
+  printf("Testing realloc:\n");
+  return 0;
+}
