41d381ac67a3c595ca6a8c2ceaba5d58f0b90c24
Meredith <mhu2@stuy.edu>
2018-10-04 20:55:17 -0400

    added tests for funcs

--- heap_funcs/heap_funcs.c
+++ heap_funcs/heap_funcs.c
@@ -6,11 +6,22 @@ int main(){
   /*make a pointer to an array */
   int *array;
   
-  printf("Testing calloc:\n");
+  printf("\nTesting calloc by making a five-int size array:\n");
   /*testing calloc, a function that dynamically allocates memory and sets it to 0*/
-  
+  array = (int*)calloc(5, sizeof(int));
+  for(int i=0;i<5;i++){
+    printf("%d",*(array+i));
+  }
   
   printf("\n\n----------------\n\n");
-  printf("Testing realloc:\n");
+  printf("Testing realloc by making the array larger and setting each value to 37:\n");
+  int *new = (int *)realloc(array, sizeof(int)*10);
+  for(int i=0;i<10;i++){
+    *(new+i)= 37;
+    printf("%d, ",*(new+i));
+  }
+
+  printf("\n\n");
+  
   return 0;
 }
